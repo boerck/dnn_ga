49edaf8efad856dd5794fd938872881dc2bcda28
re-arep <36735940+re-arep@users.noreply.github.com>
2020-08-06 21:28:05 +0900

    Modify code and annotation to the recommended format. But I don't know what "num_dnn" in line 101 means.

--- Genetic_Algorithm_part.py
+++ Genetic_Algorithm_part.py
@@ -1,19 +1,19 @@
-#import
+# import
 import random as rd
 import numpy as np
 from operator import itemgetter
 import math
 
-#math function
-def mean(values):
+
+def mean(values):  # math function
     if len(values) == 0:
         return None
     return sum(values, 0.0) / len(values)
 
-def standardDeviation(values, option = 0):
+
+def standardDeviation(values, option=0):
     if len(values) < 2:
         return None
-    sd = 0.0
     sum = 0.0
     meanValue = mean(values)
     for i in range(0, len(values)):
@@ -22,26 +22,29 @@ def standardDeviation(values, option = 0):
     sd = math.sqrt(sum / (len(values) - option))
     return sd
 
-#dnn class_Makeshift
-class custom_dnn():
-    def __init__(self,n,h):
+
+class custom_dnn:  # dnn class_Makeshift
+    def __init__(self, n, h):
         self.neural = n
         self.hidden = h
         self.accuracy = 0
+
     def Get_neural(self):
         return self.neural
+
     def Get_hidden(self):
         return self.hidden
+
     def Get_accuracy(self):
-        for i in range(10):
-            #step
+        for i in range(10): # step
             print("End")
         return self.accuracy
         
-
-#dnn_list(n) custom_dnn class의 list
-#stack(4*n) 0: num of DNN 1: neural num.list 2: hidden num.int 3: stack
-#상위 10개 하위 30% (추후 조정)
+  """
+  dnn_list(n) custom_dnn class의 list
+  stack(4*n) 0: num of DNN 1: neural num.list 2: hidden num.int 3: stack
+  상위 10개 하위 30% (추후 조정)
+  """
 def descen_create(stack,dnn_list):
     stck = sorted(stack, key=itemgetter(3))
     descen_hidden_list = []
@@ -50,7 +53,7 @@ def descen_create(stack,dnn_list):
     for i in range(10):
         descen_hidden_list.append(stck[-10:][i][2])
     descen_hidden_mean = mean(descen_hidden_list)
-    descen_hidden_standardDeviation = standardDerivation(descen_hidden_list,0)   
+    descen_hidden_standardDeviation = standardDeviation(descen_hidden_list,0)
     for t in range(int(num_dnn/3-1)):
         descen_hidden = int(descen_hidden_standardDeviation * np.random.randn() + descen_hidden_mean)
         descen_neural_sample_1, descen_neural_sample_2 = rd.sample(stck[-10:],2)
@@ -68,13 +71,13 @@ def descen_create(stack,dnn_list):
         update_stack(stack,int(stck[t][0]),descen_neural,descen_hidden)
     reset_stack(stack)
     
-#stack create,reset,update 
-def create_stack(dnn_list):
+
+def create_stack(dnn_list):  # stack create,reset,update
     stack = [ [i] for i in range(len(dnn_list))]
     for i in range(len(dnn_list)):
         stack[i].append(dnn_list[i].Get_neural)
         stack[i].append(dnn_list[i].Get_hidden)
-        Stack[i].append(0)
+        stack[i].append(0)
     return stack
 
 def reset_stack(stack):
@@ -85,10 +88,12 @@ def update_stack(stack,position,neural,hidden):
     stack[position][1] = neural
     stack[position][2] = hidden
     stack[position][3] = 0
-    
-# 50% - stack
-# accuracy,speed - stack(r) return, if num = 2 then test two simultaneously
-#reward 0:neural_num 1:time_reward 2:accuracy_reward 3: ...
+
+  """
+  50% - stack
+  accuracy,speed - stack(r) return, if num = 2 then test two simultaneously
+  reward 0:neural_num 1:time_reward 2:accuracy_reward 3: ...
+  """
 def gene_stack(reward,r,num = 1):
     for i in range(num):
         rewd = sorted(reward, key=itemgetter(i+1))
