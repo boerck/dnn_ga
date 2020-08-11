4848c6daab5bbeaa6525a28b97a568e6e66c1fcb
boerck <ohjjj1007@naver.com>
2020-08-11 21:47:47 +0900

    Change main process

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from grayscale_colorization import binary_util
 train_data = binary_util.load("data_batch_train", 1)
 test_data = binary_util.load("data_batch_test", 1)
 # batch size = 1로 설정함.
+
 model = dnn.Net(4, [32*32, 256, 64, 16, 10], train_data, test_data, 100, hin=32*32, hout=10)
-model.run()
+
 
 if __name__ == "__main__":
-    model
+    model.run()
