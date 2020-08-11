00ba1df6167fa25fddc0698222b098535cb30dff
adex1248 <40044759+adex1248@users.noreply.github.com>
2020-08-11 15:06:31 +0900

    change data in label to int64

--- grayscale_colorization/binary_util.py
+++ grayscale_colorization/binary_util.py
@@ -10,10 +10,10 @@ def load(name1: str, batch_size=1):
     with open(str('img/'+name1+'.bin'), 'rb') as f:
         loaded_data = pickle.load(f)
 
-    data = {'image': [], 'label': []}
+    #label의 데이터를 int로 변환
+    data = {'image': [], 'label': torch.zeros((len(loaded_data['image']) // batch_size, batch_size), dtype = torch.int64)}
     
     for i in range(len(loaded_data['image']) // batch_size):   #batch_size에 맞게 변환
         data['image'].append(torch.reshape(loaded_data['image'][i * batch_size: (i + 1) * batch_size], (batch_size,1024)))
-        #2차원 tensor로 변환
-        data['label'].append(loaded_data['label'][i * batch_size: (i + 1) * batch_size])
+        data['label'][i] = (loaded_data['label'][i * batch_size: (i + 1) * batch_size])
     return data
