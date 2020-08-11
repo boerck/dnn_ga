c08d62683173008ecc5dd6d707a785519eeaba96
frogyunmax <40256530+frogyunmax@users.noreply.github.com>
2020-08-11 15:26:12 +0900

    import converted data and execute training

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import Genetic_Algorithm_part as gap
 import dnn
-from grayscale_colorization import binary_util as butil
+import binary_util
 
-model = dnn.Net()
-
+data = binary_util.load("data_batch_train", 1)
+model = dnn.Net(4, [32*32, 256, 64, 16, 10], data, data, 100, hin=32*32, hout=10)
+model.run()
 gap.descen_create()
 
 if __name__ == "__main__":
