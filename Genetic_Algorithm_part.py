ddd6ddf77b82921f5d9c42e4a18e86446936dd6a
re-arep <36735940+re-arep@users.noreply.github.com>
2020-08-12 02:40:58 +0900

    Remake Genetic_Algorithm_part.py

--- Genetic_Algorithm_part.py
+++ Genetic_Algorithm_part.py
@@ -1,140 +1,122 @@
 import random as rd
 import numpy as np
 from operator import itemgetter
-import math
+import dnn
+from grayscale_colorization import binary_util as butil
 
 
-# dnn_list(n) custom_dnn class의 list
-# stack(5*n) 0: num of DNN 1: neural num.list 2: hidden num.int 3: batch size 4:stack
-# 상위 10개 하위 30% (추후 조정)
+class genetic:
+    def __init__(self, model_n):
+        self.model_n = model_n
+        self.model_list = [[0]] * model_n
 
-def descen_create(param_stack, dnn_list):
-    stck = sorted(stack, key=itemgetter(4))
-    #descen_batch_size_list = []
-    #descen_hidden_list = []
-    num_dnn = len(stack)
-    #int(num_dnn/5-1)
-    #for i in range(10):
-    #    descen_batch_size_list.append(stck[-10:][i][3])
-    #    descen_hidden_list.append(stck[-10:][i][2])
-    #descen_hidden_mean = np.mean(descen_hidden_list)
-    #descen_hidden_standardDeviation = np.std(descen_hidden_list)   
-    #descen_batch_size_mean = np.mean(descen_hidden_list)
-    #descen_batch_size_standardDeviation = np.std(descen_hidden_list)   
-    for t in range(int(num_dnn/3-1)):
-        descen_neural_sample_1, descen_neural_sample_2 = rd.sample(stck[-10:],2)
-        ln1,ln2 = [len(descen_neural_sample_1),len(descen_neural_sample_2)]
-        s,m = [ln2,ln1] if ln1>ln2 else [ln1,ln2]
-        s_sample= descen_neural_sample_2 if ln1>ln2 else descen_neural_sample_1
-        descen_neural = []
-        if rd.random()<0.1: # mutant index = 0.1
-            descen_batch_size = rd.randint(1,8)
-        else:
-            if rd.random()<0.33:
-                descen_batch_size = descen_neural_sample_1[3] 
-            elif rd.random()<0.5:
-                descen_batch_size = descen_neural_sample_2[3] 
+    def start(self):  # First Step
+        rt = []
+        for i in range(self.model_n):
+            param1 = rd.randint(2, 5)  # hidden_layer_num
+            param2 = []  # neural_num
+            param3 = rd.randint(1, 8)  # batch size
+
+            for j in range(param1):
+                param2.append(rd.randint(16, 32 * 32))
+
+            train_data = butil.load("trainset", param3)
+            test_data = butil.load("testset", param3)
+            epoch = 5
+            rt.append([dnn.Net(param1, param2, train_data, test_data, epoch), [param1, param2, param3]])
+        return rt
+
+    def create(self, param_list):
+        rt = []
+        for i in range(self.model_n):
+            ns1, ns2 = rd.sample(param_list, 2)
+            ln1 = ns1[0]
+            ln2 = ns2[0]
+            b, s, b_sample = [ln1, ln2, ns1] if ln1 >= ln2 else [ln2, ln1, ns2]
+            neural = []
+            if rd.random() < 0.05:  # mutant index = 0.05
+                batch_size = rd.randint(1, 8)
             else:
-                descen_batch_size = int((descen_neural_sample_1[3] + descen_neural_sample_2[3])/2)
-        if rd.random()<0.1: # mutant index = 0.1
-            descen_hidden = rd.randint(2,5)
-        else:
-            if rd.random()<0.33:
-                descen_hidden = descen_neural_sample_1[1]
-            elif rd.random()<0.5:
-                descen_hidden = descen_neural_sample_2[1]
-            else:
-                descen_hidden = int((descen_neural_sample_1[1] + descen_neural_sample_2[1])/2)
-        for i in range(m):
-            if i < s:
-                if rd.random() < 0.01:  # mutant index = 0.1
-                    neural.append(rd.randint(16, 32 * 32))
+                rate = rd.random()
+                if rate > 0.66:
+                    batch_size = ns1[2]
+                elif rate > 0.33:
+                    batch_size = ns2[2]
                 else:
-                    neural.append(
-                        int(abs(neural_sample_1[i] - neural_sample_2[i]) / 4 * np.random.randn() \
-                            + mean([neural_sample_1[i], neural_sample_2[i]])))
+                    batch_size = int((ns1[2] + ns2[2]) / 2)
+
+            if rd.random() < 0.05:  # mutant index = 0.05
+                hidden = rd.randint(2, 5)
             else:
-                if rd.random() < 0.01:  # mutant index = 0.1
-                    neural.append(rd.randint(16, 32 * 32))
+                rate = rd.random()
+                if rate > 0.66:
+                    hidden = ns1[0]
+                elif rate > 0.33:
+                    hidden = ns2[0]
                 else:
-                    neural.append(s_sample[i])
-        train_data, test_data, epoch = < 데이터
-        불러오기 > (by batch_size)
-        dnn_list[int(stack[t][0])] = Net(neural, hidden, train_data, test_data, epoch)
-        update_stack(stack, int(stack[t][0]), neural, hidden)
-    reset_stack(stack)
+                    hidden = int((ns1[0] + ns2[0]) / 2)
 
+            for j in range(hidden):
+                if j < s:
+                    if rd.random() < 0.05:  # mutant index = 0.05
+                        neural.append(rd.randint(16, 32 * 32))
+                    else:
+                        neural.append(
+                            int(np.std([ns1[1][j], ns2[1][j]]) * np.random.randn() + np.mean([ns1[i], ns2[i]])))
 
-# stack create,reset,update
-def create_stack(dnn_list):
-    stack = [[i] for i in range(len(dnn_list))]
-    for i in range(len(dnn_list)):
-        stack[i].append(dnn_list[i].Get_neural)
-        stack[i].append(dnn_list[i].Get_hidden)
-        Stack[i].append(0)
-    return stack
+                else:
+                    if rd.random() < 0.01:  # mutant index = 0.1
+                        neural.append(rd.randint(16, 32 * 32))
+                    else:
+                        neural.append(b_sample[i])
 
+            train_data = butil.load("trainset", batch_size)
+            test_data = butil.load("testset", batch_size)
+            epoch = 5
+            rt.append([dnn.Net(hidden, neural, train_data, test_data, epoch), [hidden, neural, batch_size]])
+        return rt
 
-def reset_stack(stack):
-    for i in stack:
-        i[3] = 0
+    def stack(self, result_list, num=3):
+        stack = [0] * self.model_n
 
+        for i in range(num):
+            rewd = sorted(result_list, key=itemgetter(i + 1))
 
-def update_stack(stack, position, neural, hidden):
-    stack[position][1] = neural
-    stack[position][2] = hidden
-    stack[position][3] = 0
+            for j in rewd[0:(self.model_n / 2)]:
+                if i == 0:
+                    stack[int(j[0])] += 1  # accuracy
+                elif i == 1:
+                    stack[int(j[0])] += 0.5  # train time
+                else:
+                    stack[int(j[0])] += 0.1  # test time
 
+        return stack
 
-# -----------------------------------------------code start
+    def competition(self, model_list):
+        active_table = [1] * self.model_n
+        stack_table = [0] * self.model_n
 
-def start(num):  # First Step
-    rt = []
-    for i in range(num):
-        param2 = []  # neural_num
-        param1 = rd.randint(2, 5)  # hidden_num
-        param3 = rd.randint(1, 8)  # batch size
-        for j in range(param2):
-            param1.append(rd.randint(16, 32 * 32))
-        train_data, test_data, epoch = < 데이터
-        불러오기 > (by param3)
-        rt.append(Net(param1, param2, train_data, test_data, epoch))
-    return rt
+        while True:
+            result_list = [[i] for i in range(self.model_n)]
 
+            for i in range(self.model_n):
+                if active_table[i]:
+                    param = model_list[i][0].run()
+                    result_list[i].append(param[3])
+                    result_list[i].append(param[4])
+                    result_list[i].append(param[5])
 
-# Second Step_(run - #stack) * n
-# 50% - stack
-# accuracy,speed - stack(r) return, if num = 3 then test three simultaneously
-# reward 0:neural_num 1:time_reward 2:accuracy_reward 3: ...
+            stack_table += genetic.stack(result_list, 3)
 
-def gene_stack(reward, r, num=3):
-    for i in range(num):
-        rewd = sorted(reward, key=itemgetter(i + 1))
-        stack = np.zeros(len(reward))
-        for i in rewd[0:int(len(reward) / 2)]:
-            stack[int(i[0])] += r
-    return stack
+            active_n = 0
+            for j in range(self.model_n):
+                if stack_table[j] >= 5:
+                    active_table[j] = 0
+                else:
+                    active_n += 1
 
+            if active_n <= (self.model_n * 0.3):
+                break
 
-# num: stack 반복횟수
-
-def Gene(num, list_DNN)
-    stack_total = np.zeros(len(reward))
-    num_dnn = len(list_DNN)
-    param_list = [[i] for i in range(num_dnn)]
-    for i in range(num):
-        result_list = [[i] for i in range(num_dnn)]
-        for j in range(len(list_DNN)):
-            param = list_DNN[j].run()
-            result_list[j].append(param[3])
-            result_list[j].append(param[4])
-            result_list[j].append(param[5])
-            if i == 0:
-                param_list[j].append(param[0])
-                param_list[j].append(param[1])
-                param_list[j].append(param[2])
-        stack_total += gene_stack(result_list, 1, 3)
-    param_stack = [i for i in param_list]
-    for i in stack_total:
-        param_stack.append(i)
-    descen_create(param_stack, list_DNN)
+        param_list = [i[1] for i in model_list]
+        return param_list
