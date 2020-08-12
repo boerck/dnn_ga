d83dc23023e1b63357fe7342e5040b1379cf92e5
re-arep <36735940+re-arep@users.noreply.github.com>
2020-08-12 10:51:12 +0900

    Completing the "main.py" code

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-#import Genetic_Algorithm_part as gap
-import dnn
+import Genetic_Algorithm_part as gap
 from grayscale_colorization import binary_util, grayscale_colorization
 
 binary_util.save("data_batch_train", grayscale_colorization.grayscale(True))
 binary_util.save("data_batch_test", grayscale_colorization.grayscale(False))
 #binary save
 
-train_data = binary_util.load("data_batch_train", 1)
-test_data = binary_util.load("data_batch_test", 1)
-# batch size = 1로 설정함.
-
-model = dnn.Net(4, [32*32, 256, 64, 16, 10], train_data, test_data, 100, hin=32*32, hout=10)
-
 
 if __name__ == "__main__":
-    model.run()
+    genetic = gap.genetic(200)
+    model = genetic.start()
+    k = 0
+    epoch = 15
+    for i in range(epoch):
+        k += 1
+        su_model = genetic.competition(model)
+        model = genetic.create(su_model)
+
+        if k == epoch:
+            print(su_model)
+
